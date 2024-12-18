06acc35bc2f6dda8eadad6133da882d2107efc91
Arzhan <chilchinov99@mail.ru>
2024-12-18 14:04:18 +0300

    upload

---
+++ src/mock/workouts.js
@@ -0,0 +1,18 @@
+const workouts = [
+  {
+    id: 1,
+    sport: "running",
+    duration: 12,
+    intensity: "medium",
+    date: "2023-10-05",
+  },
+  {
+    id: 2,
+    sport: "cycling",
+    duration: 13,
+    intensity: "high",
+    date: "2023-10-06",
+  },
+];
+
+export { workouts };
